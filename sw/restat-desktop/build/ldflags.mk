5a02e77c682dd9436895d068de57ba19c182efda
Vladimir Roncevic <elektron.ronca@gmail.com>
2021-12-14 00:05:45 +0100

    [restat] #8 initial project setup for restat-desktop

---
+++ sw/restat-desktop/build/ldflags.mk
@@ -0,0 +1,9 @@
+#
+# @brief   restat-desktop
+# @version 1.0.0
+# @date    Mon 13 Dec 2021 11:45:04 PM CET
+# @company None, free software to use 2021
+# @author  Vladimir Roncevic <elektron.ronca@gmail.com>
+#
+
+LDFLAGS=
